37d52612b73143c684e0c89f1f03cf561d90ceb7
Marcelo <marcelo.arraes@gmail.com>
2024-02-25 08:40:39 +1030

    feat: add first sound on click of button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,13 @@
-import * as React from "react";
+"use client";
+import dynamic from "next/dynamic";
+import React, { useEffect, useState } from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../../lib/utils";
+import useSound from "use-sound";
+import buttonSound from "../../sounds/button.mp3";
+
+// button.mp3
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300",
@@ -46,10 +52,23 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+
+    const [play, { stop }] = useSound("/button.mp3", { volume: 0.5 });
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+      if (isPlaying) {
+        play();
+      } else {
+        stop();
+      }
+    }, [isPlaying, play, stop]);
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        onClick={() => setIsPlaying(!isPlaying)}
         {...props}
       />
     );
